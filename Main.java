5ecbe91510e072d69c9f9048531539646041c0ab
copycat13 <zhohan.vsevolod@gmail.com>
2020-08-21 17:20:49 +0300

    HELP command added

--- Main.java
+++ Main.java
@@ -12,27 +12,39 @@ public class Main {
             System.out.print("Input command: ");
             command = scanner.nextLine().split(" ");
 
-            if (!"exit".equals(command[0])) {
-                if (command.length != 3) {
-                    System.out.println("Bad parameters!");
-                    continue;
-                }
+            if ("exit".equals(command[0].toLowerCase())) {
+                break;
             }
 
-            if ("start".equals(command[0])) {
-                players[0] = Player.of(command[1]);
-                players[1] = Player.of(command[2]);
+
+            if ("start".equals(command[0].toLowerCase())) {
+                if (command.length != 3) {
+                    System.out.println("Bad parameters!\nCorrect input: START [MODE] [MODE]");
+                    continue;
+                }
+                players[0] = Player.of(command[1].toLowerCase());
+                players[1] = Player.of(command[2].toLowerCase());
 
                 if (players[0] == null || players[1] == null) {
-                    System.out.println("Bad parameters!");
+                    System.out.println("Bad parameters: input correct players!\nExample: START USER MEDIUM");
                     continue;
                 }
 
                 Game game = new Game();
                 game.startGame(players[0], players[1]);
-                System.out.println("To exit the game input \"exit\"");
+                System.out.println("To exit the game input EXIT");
+                continue;
             }
-        } while (!command[0].equals("exit"));
+
+
+            if ("help".equals(command[0].toLowerCase())) {
+                System.out.println("START [MODE] [MODE]\tstart the game.\n  MODE: USER, EASY, MEDIUM, HARD");
+                System.out.println("EXIT\texit the game");
+                continue;
+            }
+
+            System.out.println("Incorrect command. List of commands: HELP");
+        } while (true);
     }
 
 }
